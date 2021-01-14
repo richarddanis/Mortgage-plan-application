8f0bef15fd7107359514c1f0db27ea8b80322d4d
Richard Danis <richard_danis@epam.com>
2021-01-14 14:04:01 +0100

    Extened test coverage to calcualor and prospect resolver

--- src/test/java/calculator/MortgageCalculatorTest.java
+++ src/test/java/calculator/MortgageCalculatorTest.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import model.MortgageCalculation;
+import model.Prospect;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Unit test for {@link MortgageCalculator}.
@@ -17,6 +23,11 @@ public class MortgageCalculatorTest {
 
     @Test
     public void testShouldCalculateTheMortgage() {
+        List<Prospect> prospectList = Arrays.asList(new Prospect("test", 100.5, 100.2, 3), new Prospect("test-2", 200,50, 10));
+        List<MortgageCalculation> expected = Arrays.asList(new MortgageCalculation("test", 100.5, 3, 8.887108378218747), new MortgageCalculation("test-2", 200.0, 10, 8.39593952209981));
 
+        List<MortgageCalculation> actual = calculator.calculate(prospectList);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
--- src/test/java/filereader/ProspectReaderTest.java
+++ src/test/java/filereader/ProspectReaderTest.java
@@ -1,8 +1,96 @@
 package filereader;
 
+import model.Prospect;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.rules.TemporaryFolder;
+import org.mockito.Mockito;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Unit test for {@link ProspectReader}.
  */
 public class ProspectReaderTest {
 
+    private ProspectReader reader;
+    private ProspectResolver mockResolver;
+
+    private Writer writer;
+    private File file;
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Before
+    public void init() throws IOException {
+        mockResolver = Mockito.mock(ProspectResolver.class);
+        reader = new ProspectReader(mockResolver);
+        file = folder.newFile("unit-test.txt");
+        writer = new FileWriter(file);
+    }
+
+    @Test
+    public void testShouldReadAndMapGivenFileToProspectObject() throws IOException {
+        Mockito.when(mockResolver.mapFrom(Mockito.anyString())).thenReturn(new Prospect("test", 2,3.0,4));
+        writeStringToFile("first line \n test,2,3,4");
+
+        List<Prospect> expected = Collections.singletonList(new Prospect("test", 2, 3.0, 4));
+
+        List<Prospect> actual = reader.fromFile(file.getAbsolutePath());
+
+        Assertions.assertEquals(expected, actual);
+        Mockito.verify(mockResolver, Mockito.only()).mapFrom(Mockito.anyString());
+    }
+
+    @Test
+    public void testShouldSkipTheFirstLineOfFile() throws IOException {
+        writeStringToFile("first line");
+
+        List<Prospect> actual = reader.fromFile(file.getAbsolutePath());
+
+        Assertions.assertEquals(Collections.emptyList(), actual);
+        Mockito.verify(mockResolver, Mockito.never()).mapFrom(Mockito.anyString());
+    }
+
+    @Test
+    public void testShouldFilterTheEmptyObject() throws IOException {
+        Mockito.when(mockResolver.mapFrom(Mockito.anyString())).thenReturn(new Prospect("test", 2,3.0,4)).thenReturn(null);
+        writeStringToFile("first line \n test,2,3,4 \n test,2,3,4");
+
+        List<Prospect> expected = Collections.singletonList(new Prospect("test", 2, 3.0, 4));
+
+        List<Prospect> actual = reader.fromFile(file.getAbsolutePath());
+
+        Assertions.assertEquals(expected, actual);
+        Mockito.verify(mockResolver, Mockito.times(2)).mapFrom(Mockito.anyString());
+    }
+
+    @Test
+    public void testShouldCatchAndContinueTheReadWhenResolverThrownAnException() throws IOException {
+        Mockito.when(mockResolver.mapFrom(Mockito.anyString())).thenThrow(new RuntimeException()).thenReturn(new Prospect("test-2", 6,5.0,4));
+        writeStringToFile("first line \n test,2,3,4 \n test-2,6,5,4");
+        List<Prospect> expected = Collections.singletonList(new Prospect("test-2", 6.0, 5.0, 4));
+
+
+        List<Prospect> actual = reader.fromFile(file.getAbsolutePath());
+
+        Assertions.assertEquals(expected, actual);
+        Mockito.verify(mockResolver, Mockito.times(2)).mapFrom(Mockito.anyString());
+    }
+
+    private void writeStringToFile(String line) throws IOException {
+        writer.write(line);
+        writer.flush();
+        writer.close();
+    }
 }
